5eacdbd1f79ce89ebc79fdc421a4edcb9181b0c1
unknown <santos.reis.vinicius.jorge@gmail.com>
2018-11-08 01:13:46 -0200

    initial commit

---
+++ src/providers/language/language.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { LanguageModel } from "./language.model";
+
+@Injectable()
+export class LanguageService {
+  languages : Array<LanguageModel> = new Array<LanguageModel>();
+
+   constructor() {
+     this.languages.push(
+       {name: "Portugues", code: "pt"},
+       {name: "English", code: "en"},
+       {name: "Spanish", code: "es"},
+       {name: "Arabic", code: "ar"}
+     );
+   }
+
+   getLanguages(){
+     return this.languages;
+   }
+ }
